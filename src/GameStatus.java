428e58afd93caa72fed9b11c1313846fcb3fb55a
JackContois <jackc.contois@gmail.com>
2024-02-13 18:18:35 -0500

    testing features of the board

---
+++ src/GameStatus.java
@@ -0,0 +1,12 @@
+import java.awt.Color;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class GameStatus extends JPanel {
+
+	public GameStatus() {
+		//setBackground(Color.black);
+		
+	}
+}
